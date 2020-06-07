22358225ffff8991ee27bdc6d8640aea808a32a4
portagejano <Xhuljanogjoshi12@gmail.com>
2020-06-06 22:50:43 -0500

    added submit box

--- divvyproj/src/pages/map.js
+++ divvyproj/src/pages/map.js
@@ -3,7 +3,9 @@ import ReactMapGL, { Marker, Popup } from 'react-map-gl';
 import * as stationData from '../data/divvy-bike-stations.json';
 
 import PolylineOverlay from '../components/polylineOverlay.js';
-
+import Box from '@material-ui/core/Box';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 export default function Map() {
   const [viewport, setViewport] = useState({
@@ -76,6 +78,52 @@ export default function Map() {
           </Popup>
         ) : null}
       </ReactMapGL>
+      <Box
+        bgcolor="none"
+        color="white"
+        p={2}
+        position="absolute"
+        top={0}
+        right="0%"
+        height="100%"
+        width={200}
+      >
+        <Box
+          bgcolor="none"
+          color="black"
+          p={2}
+          position="absolute"
+          top={100}
+          right="0%"
+          height={200}
+          width={200}
+        >
+          Enter bike id to figure out where you went on the divvy bike
+      <TextField
+            id="standard-basic"
+            label="Enter ID"
+          />
+          <Box
+            bgcolor="grey"
+            color="white"
+            p={2}
+            position="absolute"
+            top={110}
+            right="14%"
+            height={50}
+            width={50}
+          >
+            <Button
+              anchor="right"
+              size="small"
+              variant="outlined"
+              color="primary"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </div>
   )
 }
